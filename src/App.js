f435dc67884ffd48bbf4a3036bd4d22c3b253444
ssphyss <ss.physs@gmail.com>
2018-10-18 21:34:37 +0800

    commit branch3 安裝antd 並且創建TodoList樣式

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { Button, Input, List } from 'antd';
+import 'antd/dist/antd.css';
+
+const data = [
+	'Racing car sprays burning fuel into crowd.',
+	'Japanese princess to wed commoner.',
+	'Australian walks 100km after outback crash.',
+	'Man charged over missing wedding girl.',
+	'Los Angeles battles huge wildfires.',
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello OK
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{margin: '30px'}}>
+				<div>
+					<Input placeholder='請輸入' style={{width: '300px', marginRight: '10px'}}/>
+					<Button type='primary'>提交</Button>
+				</div>
+				<List
+					style={{marginTop: '30px', width: '300px'}}
+					bordered
+					dataSource={data}
+					renderItem={item => (<List.Item>{item}</List.Item>)}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
